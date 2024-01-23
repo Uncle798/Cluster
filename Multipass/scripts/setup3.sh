0c044bd04160c2824ce23c33b1ced72d1f74e0fb
Eric Branson <eric.branson@gmail.com>
2024-01-22 18:12:53 -0700

    unneeded scripts

--- Multipass/scripts/setup3.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/zsh
-multipass launch -n test01 --cloud-init ~/documents/cluster/multipass/cloud-init.yaml
-multipass launch -n test02 --cloud-init ~/documents/cluster/multipass/cloud-init.yaml
-multipass launch -n test03 --cloud-init ~/documents/cluster/multipass/cloud-init.yaml
-multipass list
