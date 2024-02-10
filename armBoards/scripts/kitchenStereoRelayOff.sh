45756dfd99b9696948589ddd8d11eb75a912b658
Eric Branson <eric.branson@gmail.com>
2024-02-09 20:02:30 -0700

    fixed auto complete error

--- armBoards/scripts/kitchenStereoRelayOff.sh
+++ armBoards/scripts/kitchenStereoRelayOff.sh
@@ -4,6 +4,6 @@ GPIO_PIN=4
 GPIO_MODE="$(gpio mode $GPIO_PIN out)"
 GPIO_STATUS="$(gpio read $GPIO_PIN)"
 
-if [ "$GPIO_MODE" != "0" ]; then
+if [ "$GPIO_STATUS" != "0" ]; then
     gpio write $GPIO_PIN 0
 fi
--- armBoards/scripts/kitchenStereoRelayOn.sh
+++ armBoards/scripts/kitchenStereoRelayOn.sh
@@ -4,6 +4,6 @@ GPIO_PIN=4
 GPIO_MODE="$(gpio mode $GPIO_PIN out)"
 GPIO_STATUS="$(gpio read $GPIO_PIN)"
 
-if [ "$GPIO_MODE" != "1" ]; then
+if [ "$GPIO_STATUS" != "1" ]; then
     gpio write $GPIO_PIN 1
 fi
